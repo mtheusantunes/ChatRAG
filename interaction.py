32ec8c1ec98593903827564b0ab2f07ffc17bfb1
mtheusantunes <matheusanfr@gmail.com>
2026-03-25 21:40:06 -0300

    feature: implements connection to weaviate and a full RAG chat chain

--- interaction.py
+++ interaction.py
@@ -1,21 +1,56 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_weaviate.vectorstores import WeaviateVectorStore
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+import weaviate
 
+# Embedding
+embeddings = HuggingFaceEmbeddings(
+    model_name = "all-MiniLM-L6-v2",
+    model_kwargs = {'device': 'cpu'}
+)
+
+# Database connection
+weaviate_client = weaviate.connect_to_local()
+vector_store = WeaviateVectorStore(
+    embedding=embeddings,
+    client=weaviate_client,
+    index_name="StudyMaterials",
+    text_key="text"
+)
+
+retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+
+# LLM settings
 llm = ChatOllama(model="llama3", temperature=0.1)
 
+# Prompt Engineering
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "Você é um assistente de estudos prestativo especialista em Deep Learning,"
-     "LLMs, LangChain e LangGraph. Responda de forma clara, didática e sempre em português."),
-    ("human", "{pergunta}")
+    ("system", "Use os contextos abaixo para responder a questão no fim. Se não souber a resposta, "
+     "apenas diga que não sabe, não tente inventar uma resposta. Use no máximo três sentenças para "
+     "manter a resposta mais simples o possível. Responda sempre em português brasileiro."
+     "Sempre diga 'obrigado por perguntar' no fim da resposta. Contexto: {context} Pergunta: {question} "
+     "Resposta útil:"),
+    ("human", "{question}")
 ])
 
-parser = StrOutputParser()
+# Chain RAG
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
-chain = prompt | llm | parser
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
 
-user_question = input("Digite sua pergunta: ")
-print("Pensando...")
-
-for chunk in chain.stream({"pergunta":  user_question}):
+# Test
+user_question = input("Usuário: ")
+print("Thinking...")
+for chunk in rag_chain.stream(user_question):
     print(chunk, end="", flush=True)
+
+weaviate_client.close()
